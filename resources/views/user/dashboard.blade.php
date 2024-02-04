f1ba3eb6f5a7988c1f80c29f141134b99b766c9b
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2024-02-05 01:32:25 +0600

    updated

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="{{ asset('css/user.css') }}" rel="stylesheet">
     <title>Document</title>
+
 </head>
 <body>
 
@@ -18,6 +19,7 @@
             <div class="col md-12">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <img src={{ asset('images/icon1.png') }} alt="">
+                    <a> VaxGuard </a>
                     <div class="collapse navbar-collapse mr-5" id="navbarNavAltMarkup">
                         <div class="navbar-nav ml-auto">
                             <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
@@ -36,22 +38,94 @@
 
         {{--background image--}}
         <div class="content">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="col-md-3">
+                        <div class="bigButton">
+                            <button> abc </button>
+                        </div>
+                    </div>
 
+                    <div class="col-md-3">
+                        <div class="buttons">
+                            <button> abc </button>
+                            <button> bcd </button>
+                            <button> aad </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
         </div>
+
+
+
 
         {{--nicher bar ta--}}
         <div class="lowerBox">
 
         </div>
 
+        {{-- Just headline of the process--}}
+        <div class="processHeadline">
+            <div class="markBox">
+
+            </div>
+            <div class="text">
+                <h4> The process of receiving the vaccines.</h4>
+            </div>
+        </div>
+
+        {{--Process --}}
+        <div class="process">
+
+            <div class="card-deck">
+                <div class="card">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+            </div>
+
+        </div>
 
         {{--body end--}}
 
 
 
+
+
+
+
     </div>
+
+
 
 </body>
 </html>
